cbdab6bfa7ea742143e744feabce9b3a35f12a56
Jonathan Kupcho <jonathan.kupcho@kohls.com>
2018-10-08 11:02:11 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>We should really change this code!!!</p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Service Workers
+            Blarg!!
           </a>
         </header>
       </div>
